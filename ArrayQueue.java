84f03a317e8dbaee7d64e0ca82deb96ae5baa990
camdeputy <cameroncdeputy@gmail.com>
2019-10-21 22:35:30 -0700

    final

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,59 +1,36 @@
-public class ArrayQueue<String> implements  Queue<String> {
-    private static final int DEFAULT_CAPACITY = 10;
-    private int cap,
-            cur,
-            front,
-            back;
-    private String[] A;
+import java.util.Arrays;
+
+public class ArrayQueue<T> implements Queue {
+
+    private Object[] a;
+    private int front;
+    private int rear;
 
     public ArrayQueue() {
-        cap = DEFAULT_CAPACITY;
-        A = (String[]) new Object[DEFAULT_CAPACITY];
-        back = -1;
+        a = new Object[20];
         front = 0;
+        rear = 0;
+    }
+
+    public Object dequeue() throws Exception {
+        if(empty())
+            throw new Exception("Queue empty");
+        Object temp = a[front++];
+        return temp;
+    }
+
+    public void enqueue(Object item) {
+        if(rear == (a.length-1))
+            grow_array();
+        a[rear++] = item;
     }
 
     public boolean empty() {
-        return cur == 0;
+        return front == rear;
     }
 
-    public void enqueue(String value) {
-        if (isFull())
-            resize(A.length*2);
-        back++;
-        A[back % cap] = value;
-        cur++;
-    }
-
-    public String getFront() throws Exception {
-        if (empty())
-            throw new Exception();
-        else
-            return A[front % cap];
-    }
-
-    public String dequeue() throws Exception {
-        String e = getFront();
-        A[front % cap] = null;
-        front++;
-        cur--;
-        return e;
-    }
-
-    public boolean isFull() {
-        return cur == cap;
-    }
-
-    public void resize(int reSize){
-        String[] tmp = (String[]) new Object[reSize];
-
-        int current = front;
-        for (int i = 0; i < A.length; i++)
-        {
-            tmp[i] = A[current];
-            current = (current + 1) % A.length;
-        }
-        A = tmp;
-
+    void grow_array() {
+        Object[] copy = Arrays.copyOfRange(a, 0, a.length*2);
+        a = copy;
     }
 }
--- ArrayStack.java
+++ ArrayStack.java
@@ -1,55 +1,41 @@
+import java.util.Arrays;
 
-public class ArrayStack<String> implements Stack {
+public class ArrayStack<T> implements Stack {
 
-    private static final int DEFAULT_CAPACITY = 15;
-    private int top;
-    private String[] A;
-
-    public ArrayStack(int initialCapacity) {
-        if (initialCapacity <= 0)
-            A = (String[]) new Object[DEFAULT_CAPACITY];
-        else
-            A = (String[]) new Object[initialCapacity];
-
-        top = -1;   //stack is empty
-    }
+    private Object[] a;
+    private int size;
 
     public ArrayStack() {
-        this(DEFAULT_CAPACITY);
+        a = new Object[20];
+        size = -1;
     }
 
     public boolean empty() {
-        return top == -1;
+        return size == -1;
     }
 
-    public String peek() throws Exception {
-        if (empty()) throw new Exception("Stack is empty");
-        return A[top];
+    public Object peek() throws Exception {
+        if(empty())
+            throw new Exception("Stack empty");
+        return a[size];
     }
 
-    public String pop() throws Exception {
-        String x = peek();
-        A[top] = null;
-        top--;
-        return x;
+    public Object pop() throws Exception {
+        if(empty())
+            throw new Exception("Stack empty");
+        Object temp = a[size--];
+        return temp;
     }
 
-    public void push(Object e) throws Exception {
-        if (top == A.length)
-            resize(A.length*2);
-        top++;
-        A[top] = (String) e;
+    public void push(Object item) throws Exception {
+        if(size == (a.length-1))
+            grow_array();
+        a[++size] = item;
     }
 
-    private void resize(int capacity) {
-        assert capacity >= A.length;
-
-        // textbook implementation
-        String[] temp = (String[]) new Object[capacity];
-        for (int i = 0; i < A.length; i++) {
-            temp[i] = A[i];
-        }
-        A = temp;
+    private void grow_array() {
+        Object[] copy = Arrays.copyOfRange(a, 0, a.length*2);
+        a = copy;
     }
 
 }
